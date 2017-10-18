29ed08a8643793e60976a01963544d564b52d102
Fernando Pinheiro Campos <fpinheirocampos@gmail.com>
2017-10-18 12:55:45 -0200

    Criado componente de fotos

---
+++ src/foto/FotoComponent.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: `foto`,
+    template: `<img width="600" src="assets/img/special.png" />  `
+})
+export class FotoComponent{}
---
+++ src/foto/FotoModule.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { FotoComponent } from './FotoComponent';
+
+@NgModule ({
+    declarations: [FotoComponent],
+    exports:[FotoComponent]
+})
+export class FotoModule{}
